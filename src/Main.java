3f00c2cf95ab977a69f9ac789c9e33e0431bde56
abubakar <dadaev1098@mail.ru>
2023-08-04 23:22:34 +0300

    хумид

---
+++ src/Main.java
@@ -0,0 +1,100 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        tasck2();
+        tasck3();
+        tasck4();
+        tasck5();
+        tasck6();
+        tasck8();
+    }
+
+    public static void task1() {
+        System.out.println("задача 1");
+        var dog = 8.0;
+        System.out.println(dog);
+        var box = dog + 4;
+        System.out.println(box);
+        box = box - 3.5;
+        System.out.println(box);
+    }
+
+    public static void tasck2() {
+        System.out.println("задача 2");
+        var cat = 3.6;
+        System.out.println(cat);
+        var box = cat + 4;
+        System.out.println(box);
+        box = box - 1.6;
+        System.out.println(box);
+    }
+
+    public static void tasck3() {
+        System.out.println("задача 3");
+        var paper = 763789;
+        System.out.println(paper);
+        var box = paper + 4;
+        System.out.println(box);
+        box = box - 7639;
+        System.out.println(box);
+    }
+
+    public static void tasck4() {
+        System.out.println("Ззадача 4");
+        var friend = 19;
+        System.out.println(friend);
+        var box = friend + 2;
+        System.out.println(box);
+        box = box / 7;
+        System.out.println(box);
+
+    }
+
+    public static void tasck5() {
+        System.out.println("задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        var box = frog * 10;
+        System.out.println(box);
+        box =box / 3.5;
+        System.out.println(box);
+        box =box + 4;
+        System.out.println(box);
+}
+    public static void  tasck6 () {
+        System.out.println("задача 6");
+        var boxer = 78.2;
+        var boxer2 = 82.7;
+       System.out.println(boxer2 + boxer);
+       System.out.println(boxer2 - boxer);
+       System.out.println(boxer2 % boxer);
+
+
+    }
+    public static void  tasck8() {
+        var hours = 640;
+        var employees = hours / 8;
+        System.out.println("Всего работников в компании - " +  employees + " человек");
+
+        employees = 174;
+        hours = 174 * 8;
+        System.out.println("Если в компании работает " + employees +" человек, то всего " + hours + " часов работы может быть поделено между сотрудниками");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
